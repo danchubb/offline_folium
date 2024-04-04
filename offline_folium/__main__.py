8934fbbac42996d20aa3b78578e788862519b4a0
D-Daniel Chubb <DEV\d-chubbd@a-1ya29n2zbp45m.dev.dacc.phz>
2024-04-04 14:18:09 +0100

    added new plugindownload ability

--- offline_folium/__main__.py
+++ offline_folium/__main__.py
@@ -1,22 +1,34 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 import os
 
 import folium
+import folium.plugins as plugins 
 from .paths import dest_path
 
 def download_all_files():
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
     for _, js_url in folium.folium._default_js:
-        download_url(js_url)
+        download_url(js_url,js_url)
     for _, js_url in folium.folium._default_css:
-        download_url(js_url)
+        download_url(js_url,js_url)
+    for plugin in plugins.__all__:   
+        if hasattr(getattr(plugins,plugin),'default_js'):
+            for _, js_url in getattr(plugins,plugin).default_js:
+                req = Request(url=js_url, headers=headers) 
+                download_url(js_url,req)          
+        if hasattr(getattr(plugins,plugin),'default_css'):
+            for _, js_url in getattr(plugins,plugin).default_css:
+                req = Request(url=js_url, headers=headers)
+                download_url(js_url,req)   
 
 
-def download_url(url):
+#added a url and d(ownload)url so that browser spoofing works where reauired for the plugins
+def download_url(url,durl):
     output_path = os.path.join(dest_path, os.path.basename(url))
     print(f"Downloading {output_path}")
-    contents = urlopen(url).read().decode("utf8")
+    contents = urlopen(durl).read().decode("utf8")
     with open(output_path, "w") as f:
         f.write(contents)
 
